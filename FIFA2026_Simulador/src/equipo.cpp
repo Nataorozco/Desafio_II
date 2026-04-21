af8b9514a86e7063837b9b076e5a063173869135
Nataly Orozco <natalyorozco550@gmail.com>
2026-04-21 18:26:40 -0500

    implementar estructura básica de clase Vector

---
+++ FIFA2026_Simulador/src/equipo.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "vector.h"
+#include "equipo.h"
+using namespace std;
+
+Equipo::Equipo() {
+
+}
---
+++ FIFA2026_Simulador/src/jugador.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "vector.h"
+#include "jugador.h"
+using namespace std;
+
+jugador::jugador() {
+
+}
